e221f34de1b71824b572af107518f45fd974b241
dojh111 <dojh111@gmail.com>
2020-09-28 22:04:41 +0800

    Add Ui class to LifEasier

--- src/main/java/seedu/lifeasier/LifEasier.java
+++ src/main/java/seedu/lifeasier/LifEasier.java
@@ -2,20 +2,34 @@ package seedu.lifeasier;
 
 import java.util.Scanner;
 
+/**
+ * LifEasier is a CLI application that allows busy CEG students to schedule their day faster than traditional
+ * GUI based apps if they can type fast
+ */
 public class LifEasier {
+
+    private Ui ui;
+
+    public LifEasier(String filePath) {
+        ui = new Ui();
+    }
+
+    /**
+     * Runs the LifEasier program infinitely until termination by the user
+     */
+    public void run() {
+        boolean isFinished = false;
+
+        ui.showWelcomeMessage();
+
+        while (!isFinished) {
+        }
+    }
+
     /**
      * Main entry-point for the java.lifeasier.LifEasier application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        new LifEasier("saveFile.txt");
     }
 }
---
+++ src/main/java/seedu/lifeasier/Ui.java
@@ -0,0 +1,8 @@
+package seedu.lifeasier;
+
+
+/**
+ * The Ui class deals with all interactions with the user
+ */
+public class Ui {
+}
