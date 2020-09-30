a9fbd5ef987550985c2da0f976a8965aa6324f90
EdmundEXE <edmundexe98@gmail.com>
2020-09-30 14:05:58 +0800

    changes

---
+++ src/main/java/seedu/lifeasier/notes/Note.java
@@ -0,0 +1,25 @@
+package seedu.lifeasier.notes;
+
+public class Note {
+    private String title;
+    private String description;
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public String toString() {
+        return "Title: " + title +
+                "\n\n" + description;
+    }
+}
