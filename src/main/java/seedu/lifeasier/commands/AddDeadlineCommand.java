d556fa747fd6d5e422d267f218e1e5decec85ab8
daniellimzj <daniellimzijie@gmail.com>
2020-10-04 11:55:25 +0800

    Add AddDeadlineCommand class

---
+++ src/main/java/seedu/lifeasier/commands/AddDeadlineCommand.java
@@ -0,0 +1,15 @@
+package seedu.lifeasier.commands;
+
+import java.time.LocalDateTime;
+
+public class AddDeadlineCommand extends Command{
+
+    private String description;
+    private LocalDateTime by;
+
+    public AddDeadlineCommand(String description, LocalDateTime by) {
+        this.description = description;
+        this.by = by;
+    }
+
+}
