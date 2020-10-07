0dab3354369d6508ca1d19816dbd383eb6ca7c9a
EdmundEXE <edmundexe98@gmail.com>
2020-10-07 20:05:18 +0800

    add condition where there is an empty title

--- src/main/java/seedu/lifeasier/commands/AddNotesCommand.java
+++ src/main/java/seedu/lifeasier/commands/AddNotesCommand.java
@@ -17,8 +17,18 @@ public class AddNotesCommand extends Command {
     public void execute(Ui ui, NoteList notes, TaskList tasks) {
         ui.showNoteTitleMessage();
         String noteTitle = ui.readCommand();
+
+        while (noteTitle.trim().length() == 0) {
+            System.out.println("Empty description! =O\n");
+            noteTitle = ui.readCommand();
+        }
         ui.showNoteDescriptionMessage();
         String noteDescription = ui.readCommand();
+
+        while (noteDescription.trim().length() == 0) {
+            System.out.println("Empty description! =O\n");
+            noteDescription = ui.readCommand();
+        }
         notes.add(new Note(noteTitle,noteDescription));
         System.out.println("Ok! I've taken note of this note!\n");
     }
