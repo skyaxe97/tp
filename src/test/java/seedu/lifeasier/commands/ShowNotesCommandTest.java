59050b3cfc97646dca990c4f4bd0b9c86537d8ef
EdmundEXE <edmundexe98@gmail.com>
2020-10-10 23:23:18 +0800

    fix Junit showNotes

--- src/test/java/seedu/lifeasier/commands/ShowNotesCommandTest.java
+++ src/test/java/seedu/lifeasier/commands/ShowNotesCommandTest.java
@@ -19,6 +19,8 @@ class ShowNotesCommandTest {
 
     @Test
     void execute() {
+        notes.add(new Note("cat", "i like cats"));
+        notes.add(new Note("dog", "i like dogs"));
         assertEquals(2, notes.size());
     }
 }
