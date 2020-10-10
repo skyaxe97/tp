bab4f309c51be43724264746ec306fa0ea544187
EdmundEXE <edmundexe98@gmail.com>
2020-10-10 23:50:49 +0800

    fix checkstyle error

--- src/test/java/seedu/lifeasier/commands/ShowNotesCommandTest.java
+++ src/test/java/seedu/lifeasier/commands/ShowNotesCommandTest.java
@@ -1,6 +1,5 @@
 package test.java.seedu.lifeasier.commands;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import seedu.lifeasier.notes.NoteList;
 import seedu.lifeasier.notes.Note;
