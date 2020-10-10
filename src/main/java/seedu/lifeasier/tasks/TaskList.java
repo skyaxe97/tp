afbb8676091dcc54bfeecb718ecb123ff95b696f
skyaxe97 <skyaxe97@gmail.com>
2020-10-10 16:20:22 +0800

    Fix checkstyle

--- src/main/java/seedu/lifeasier/tasks/TaskList.java
+++ src/main/java/seedu/lifeasier/tasks/TaskList.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class TaskList {
     protected static ArrayList<Task> taskList;
     protected static int taskCount = 0;
+
     public TaskList() {
         taskList = new ArrayList<>();
     }
@@ -18,39 +19,39 @@ public class TaskList {
         return taskCount;
     }
 
-    public static void addTask (Task task) {
+    public static void addTask(Task task) {
         taskList.add(task);
         taskCount++;
     }
 
     /**
-     * Add new event to taskList
-     * @param description description of event
-     * @param start start date/time of event
-     * @param end end date/time of event
+     * Add new event to taskList.
+     * @param description description of event.
+     * @param start start date/time of event.
+     * @param end end date/time of event.
      */
-    public static void addEvent (String description, LocalDateTime start, LocalDateTime end) {
+    public static void addEvent(String description, LocalDateTime start, LocalDateTime end) {
         Event event = new Event(description, start, end);
         addTask(event);
     }
 
     /**
-     * Add new Lesson to taskList
-     * @param moduleCode module code of lesson
-     * @param start start date/time of lesson
-     * @param end end date/time of lesson
+     * Add new Lesson to taskList.
+     * @param moduleCode module code of lesson.
+     * @param start start date/time of lesson.
+     * @param end end date/time of lesson.
      */
-    public static void addLesson (String moduleCode, LocalDateTime start, LocalDateTime end) {
+    public static void addLesson(String moduleCode, LocalDateTime start, LocalDateTime end) {
         Lesson lesson = new Lesson(moduleCode, start, end);
         addTask(lesson);
     }
 
     /**
-     * Adds a new Deadline to taskList
-     * @param description description of task
-     * @param by deadline of task
+     * Adds a new Deadline to taskList.
+     * @param description description of task.
+     * @param by deadline of task.
      */
-    public static void addDeadline (String description, LocalDateTime by) {
+    public static void addDeadline(String description, LocalDateTime by) {
         Deadline deadline = new Deadline(description, by);
         addTask(deadline);
     }
