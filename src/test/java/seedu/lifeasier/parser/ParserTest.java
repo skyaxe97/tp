05bdc1b4e5633a26c5f9d48c262d54b4b16d5d02
daniellimzj <daniellimzijie@gmail.com>
2020-10-12 23:06:06 +0800

    Move non-specific exceptions to inside Parser class

--- src/test/java/seedu/lifeasier/parser/ParserTest.java
+++ src/test/java/seedu/lifeasier/parser/ParserTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import seedu.lifeasier.commands.AddLessonCommand;
 import seedu.lifeasier.commands.Command;
 import seedu.lifeasier.commands.HelpCommand;
+import seedu.lifeasier.ui.Ui;
 
 import java.time.format.DateTimeParseException;
 
@@ -15,30 +16,33 @@ class ParserTest {
     @Test
     void parseCommand_inputHelp_HelpCommand() throws ParserException {
         Parser parser = new Parser();
-        Command command = parser.parseCommand("help");
+        Ui ui = new Ui();
+        Command command = parser.parseCommand("help", ui);
         assertTrue(command instanceof HelpCommand);
     }
 
     @Test
     void parseCommand_inputAddLesson_AddLessonCommand() throws ParserException {
         Parser parser = new Parser();
-        Command command = parser.parseCommand("addLesson /code cg1111 /date 10-10-20 /time 10:00 /to 20:00");
+        Ui ui = new Ui();
+        Command command = parser.parseCommand("addLesson /code cg1111 /date 10-10-20 /time 10:00 /to 20:00", ui);
         assertTrue(command instanceof AddLessonCommand);
     }
 
     @Test
-    void parseCommand_inputInvalidCommand_ParserException()  throws ParserException {
+    void parseCommand_inputInvalidCommand_ParserException() {
         Parser parser = new Parser();
+        Ui ui = new Ui();
         assertThrows(ParserException.class, () -> {
-            parser.parseCommand("I want to add a lesson");
+            parser.parseCommand("I want to add a lesson", ui);
         });
     }
 
     @Test
-    void parseCommand_inputInvalidDateTime_ParserException()  throws ParserException {
+    void parseAddDeadlineCommand_inputInvalidDateTime_ParserException() {
         Parser parser = new Parser();
         assertThrows(DateTimeParseException.class, () -> {
-            parser.parseCommand("addDeadline do homework /by 10-30-40 24:67");
+            parser.parseAddDeadlineCommand("addDeadline do homework /by 10-30-40 24:67");
         });
     }
 
