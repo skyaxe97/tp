8b62c4835315e8fee6b58dc8ef979bc60601603b
daniellimzj <daniellimzijie@gmail.com>
2020-10-15 21:33:01 +0800

    Add method getTimingsOfTasks to FreeTimeCommand class

--- src/main/java/seedu/lifeasier/commands/FreeTimeCommand.java
+++ src/main/java/seedu/lifeasier/commands/FreeTimeCommand.java
@@ -2,8 +2,12 @@ package seedu.lifeasier.commands;
 
 import seedu.lifeasier.notes.NoteList;
 import seedu.lifeasier.storage.FileStorage;
+import seedu.lifeasier.tasks.Task;
 import seedu.lifeasier.tasks.TaskList;
 import seedu.lifeasier.ui.Ui;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class FreeTimeCommand extends Command {
 
@@ -11,5 +15,15 @@ public class FreeTimeCommand extends Command {
     public void execute(Ui ui, NoteList notes, TaskList tasks, FileStorage storage) {
 
         tasks.getTasksFromToday();
+
+
+    }
+
+    private ArrayList<LocalTime> getTimingsOfTasks(ArrayList<Task> tasks) {
+        ArrayList<LocalTime> timesList = new ArrayList<>();
+        for (Task task : tasks) {
+            timesList.add(task.getStart().toLocalTime());
+        }
+        return timesList;
     }
 }
