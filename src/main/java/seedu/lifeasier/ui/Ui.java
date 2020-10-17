eca8074ca6e4b5686fa779c9d029e6f9bc90be40
daniellimzj <daniellimzijie@gmail.com>
2020-10-17 12:11:40 +0800

    Fix printSleepTimeMessage method to display a reasonable sleeping amount

--- src/main/java/seedu/lifeasier/ui/Ui.java
+++ src/main/java/seedu/lifeasier/ui/Ui.java
@@ -226,7 +226,12 @@ public class Ui {
     public void printSleepTimeMessage(int earliestSleepTime, int latestWakeTime, int duration) {
         System.out.println("You have nothing on from " + earliestSleepTime + ":00 today to " + latestWakeTime
                  + ":00 tomorrow!");
-        System.out.println("You can sleep for up to " + duration + " hours!");
+
+        if (duration < 9) {
+            System.out.println("You can sleep for up to " + duration + " hours!");
+        } else {
+            System.out.println("You can sleep for the recommended 8 hours or longer!");
+        }
         printSeparator();
 
     }
