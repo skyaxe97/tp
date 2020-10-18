b8732732faf15c457fbab428c2d14cb9f01ba4a9
EdmundEXE <edmundexe98@gmail.com>
2020-10-18 21:03:30 +0800

    parserTest checkstyle

--- src/test/java/seedu/lifeasier/parser/ParserTest.java
+++ src/test/java/seedu/lifeasier/parser/ParserTest.java
@@ -43,7 +43,7 @@ class ParserTest {
         Parser parser = new Parser();
         Ui ui = new Ui();
         assertThrows(DateTimeParseException.class, () -> {
-            parser.parseAddDeadlineCommand( ui, "addDeadline do homework /by 10-30-40 24:67");
+            parser.parseAddDeadlineCommand(ui, "addDeadline do homework /by 10-30-40 24:67");
         });
     }
 
