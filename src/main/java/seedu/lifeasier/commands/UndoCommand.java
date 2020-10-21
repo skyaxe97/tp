ad7e739752e360c5c2b7644351e1e8c75b018b76
dojh111 <dojh111@gmail.com>
2020-10-21 20:39:11 +0800

    Add UndoCommand Class

---
+++ src/main/java/seedu/lifeasier/commands/UndoCommand.java
@@ -0,0 +1,15 @@
+package seedu.lifeasier.commands;
+
+import seedu.lifeasier.parser.Parser;
+import seedu.lifeasier.storage.FileStorage;
+import seedu.lifeasier.tasks.TaskList;
+import seedu.lifeasier.ui.Ui;
+import seedu.lifeasier.notes.NoteList;
+
+public class UndoCommand extends Command {
+
+    @Override
+    public void execute(Ui ui, NoteList notes, TaskList tasks, FileStorage storage, Parser parser) {
+        
+    }
+}
