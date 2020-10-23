9beae5bba20de98bf524ef28d2c102189ed2cb24
daniellimzj <daniellimzijie@gmail.com>
2020-10-23 21:17:15 +0800

    Create and update LessonTest and TaskListTest classes

--- src/test/java/seedu/lifeasier/tasks/TaskListTest.java
+++ src/test/java/seedu/lifeasier/tasks/TaskListTest.java
@@ -2,11 +2,12 @@ package seedu.lifeasier.tasks;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TaskListTest {
     TaskList taskList;
@@ -30,5 +31,26 @@ class TaskListTest {
         taskList = new TaskList();
         taskList.addTask(new Event("EXAMPLE", LocalDateTime.now(), LocalDateTime.now()));
         assertThrows(IndexOutOfBoundsException.class, () -> taskList.getTask(-1));
+    }
+
+    @Test
+    void updateTasks_outdatedLesson_movedOneWeek() {
+        taskList = new TaskList();
+        String moduleCode = "cg1111";
+
+        LocalDateTime start = LocalDateTime.of(20, 10, 22, 12, 00);
+        LocalDateTime end = LocalDateTime.of(20, 10, 22, 13, 00);
+        int recurrences = 11;
+
+        Lesson lesson = new Lesson(moduleCode, start, end, recurrences);
+
+        taskList.addTask(lesson);
+
+        taskList.updateTasks(LocalDate.of(20, 10, 23));
+
+        LocalDate expected = LocalDate.of(20, 10, 29);
+
+        assertTrue(((lesson.isHappeningOn(expected)) && (lesson.getRecurrences() == 10)));
+
     }
 }
