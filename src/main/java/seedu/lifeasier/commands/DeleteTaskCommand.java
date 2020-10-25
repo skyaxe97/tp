c8d9e27b483d997211bb947b10aff078009c6d7d
daniellimzj <daniellimzijie@gmail.com>
2020-10-25 13:52:28 +0800

    Add JavaDoc comments to DeleteTaskCommand class

--- src/main/java/seedu/lifeasier/commands/DeleteTaskCommand.java
+++ src/main/java/seedu/lifeasier/commands/DeleteTaskCommand.java
@@ -21,6 +21,15 @@ public class DeleteTaskCommand extends Command {
         this.name = name;
     }
 
+    /**
+     * Prints a list of all Tasks that match the user's input.
+     *
+     * @param tasks TaskList from which Tasks will be searched.
+     * @param ui Ui object to display messages to the user.
+     * @param type Type of task to be searched for.
+     * @param name Name of task to be searched for.
+     * @throws TaskNotFoundException If no matching tasks are found.
+     */
     private void printMatchingTasks(TaskList tasks, Ui ui, String type, String name) throws TaskNotFoundException {
         tasks.printMatchingTasks(type, name);
     }
@@ -29,10 +38,26 @@ public class DeleteTaskCommand extends Command {
         tasks.checkForIndexOutOfBounds(userInput);
     }
 
+    /**
+     * Deletes the specified Task from the TaskList.
+     *
+     * @param tasks TaskList from which task is to be deleted.
+     * @param ui Ui object to display messages to the user.
+     * @param index Index of task to be deleted.
+     */
     private void deleteTask(TaskList tasks, Ui ui, int index) {
         tasks.deleteTask(index, ui);
     }
 
+    /**
+     * Executes the delete task command and deletes the Task that the user specifies.
+     *
+     * @param ui Ui object to display messages to the user.
+     * @param notes NoteList containing user's notes.
+     * @param tasks TaskList containing user's tasks.
+     * @param storage Storage object to save tasks and notes to memory.
+     * @param parser Parser object to parse user's inputs.
+     */
     @Override
     public void execute(Ui ui, NoteList notes, TaskList tasks, FileStorage storage, Parser parser) {
         try {
