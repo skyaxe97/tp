29fa5c53535b7730232649ca1042482e743fe555
johan9se <johannine.enerio@gmail.com>
2020-11-05 22:30:20 +0800

    Add JUnit tests for TaskHistory

--- src/test/java/seedu/lifeasier/commands/UndoTaskCommandTest.java
+++ src/test/java/seedu/lifeasier/commands/UndoTaskCommandTest.java
@@ -6,6 +6,7 @@ import seedu.lifeasier.model.notes.NoteList;
 import seedu.lifeasier.model.tasks.Deadline;
 import seedu.lifeasier.model.tasks.Event;
 import seedu.lifeasier.model.tasks.Lesson;
+import seedu.lifeasier.model.tasks.Task;
 import seedu.lifeasier.model.tasks.TaskHistory;
 import seedu.lifeasier.model.tasks.TaskList;
 import seedu.lifeasier.parser.Parser;
@@ -24,6 +25,15 @@ class UndoTaskCommandTest {
     private final PrintStream originalOut = System.out;
     private final PrintStream originalErr = System.err;
 
+    private final TaskList testTaskList = new TaskList();
+    private final TaskHistory testTaskHistory = new TaskHistory();
+
+    private final LocalDateTime sampleTime1 = LocalDateTime.parse("2020-11-11T11:11");
+    private final LocalDateTime sampleTime2 = LocalDateTime.parse("2020-12-12T12:12");
+    private final Deadline testDeadlineToEdit = new Deadline("testDeadline", sampleTime1, 0);
+    private final Event testEventToDelete = new Event("testEvent", sampleTime1, sampleTime2, 1);
+    private final Lesson testLessonToEdit = new Lesson("testLesson", sampleTime1, sampleTime2, 3);
+
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
@@ -33,18 +43,11 @@ class UndoTaskCommandTest {
         System.setOut(originalOut);
         System.setErr(originalErr);
     }
-    private final TaskHistory testTaskHistory = new TaskHistory();
 
-    private final LocalDateTime sampleTime1 = LocalDateTime.parse("2020-11-11T11:11");
-    private final LocalDateTime sampleTime2 = LocalDateTime.parse("2020-12-12T12:12");
-    private final Deadline testDeadline = new Deadline("testDeadline", sampleTime1, 0);
-    private final Event testEvent = new Event("testEvent", sampleTime1, sampleTime2, 1);
-    private final Lesson testLesson = new Lesson("testLesson", sampleTime1, sampleTime2, 3);
-
-    void setUpTaskHistory() {
-        testTaskHistory.pushOldCopy(testDeadline);
-        testTaskHistory.pushOldCopy(testEvent);
-        testTaskHistory.pushOldCopy(testLesson);
+    void setUpTaskList() {
+        testTaskList.addTask(testDeadlineToEdit);
+        testTaskList.addTask(testEventToDelete);
+        testTaskList.addTask(testLessonToEdit);
     }
 
     void resetTaskHistory() {
@@ -69,8 +72,8 @@ class UndoTaskCommandTest {
         assertEquals(0, testTaskHistory.getChangeCount());
 
         command.execute(ui, notes, tasks, storage, parser, noteHistory, testTaskHistory);
-        assertEquals(ui.colourTextGreen("Nothing to undo!") +
-                System.lineSeparator()
+        assertEquals(ui.colourTextGreen("Nothing to undo!")
+                + System.lineSeparator()
                 + "==================================================================================================="
                 + "============" + System.lineSeparator(),
                 outContent.toString());
@@ -79,8 +82,18 @@ class UndoTaskCommandTest {
     }
 
     @Test
-    void executeAndGetLastTask_TaskHistoryWithThreeTasks_Event() {
+    void executeAndGetLastTask_TaskHistoryWithThreeTasks_LastTaskIsEvent() {
         setUpStreams();
+        setUpTaskList();
+
+        Task copyTestDeadline = testTaskHistory.getCurrCopyOfTaskToEdit(testTaskList,0);
+        Task copyTestEvent = testTaskHistory.getCurrCopyOfTaskToDelete(testTaskList,1);
+        Task copyTestLesson = testTaskHistory.getCurrCopyOfTaskToEdit(testTaskList,2);
+
+        testTaskHistory.pushOldCopy(copyTestDeadline);
+        testTaskHistory.pushOldCopy(copyTestEvent);
+        testTaskHistory.pushOldCopy(copyTestLesson);
+
         Ui ui = new Ui();
         NoteList notes = new NoteList();
         TaskList tasks = new TaskList();
@@ -92,13 +105,20 @@ class UndoTaskCommandTest {
         UndoTaskCommand command = new UndoTaskCommand();
 
         assertEquals(3, testTaskHistory.getChangeCount());
-        assertEquals(testLesson, testTaskHistory.getLastTask());
+        assertEquals(copyTestLesson, testTaskHistory.getLastTask());
 
         command.execute(ui, notes, tasks, storage, parser, noteHistory, testTaskHistory);
+        assertEquals(ui.colourTextGreen("This deleted task has been successfully restored!")
+                        + System.lineSeparator() + testLessonToEdit.toString()
+                        + System.lineSeparator()
+                        + "========================================================================================"
+                        + "=======================" + System.lineSeparator(),
+                outContent.toString());
 
         assertEquals(2, testTaskHistory.getChangeCount());
-        assertEquals(testEvent, testTaskHistory.getLastTask());
+        assertEquals(copyTestEvent, testTaskHistory.getLastTask());
 
+        resetTaskHistory();
         restoreStreams();
     }
 
