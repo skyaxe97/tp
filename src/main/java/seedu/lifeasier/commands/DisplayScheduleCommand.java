1a4c7036eeafd172f30bf7a4526711f7c5a87561
johan9se <johannine.enerio@gmail.com>
2020-11-08 17:14:26 +0800

    Revert "Add proper thick line and blank line separators for displaying schedule"
    This reverts commit 29e8f32a2938680052e95f4cb4e6f73a831cb18c.

--- src/main/java/seedu/lifeasier/commands/DisplayScheduleCommand.java
+++ src/main/java/seedu/lifeasier/commands/DisplayScheduleCommand.java
@@ -35,13 +35,9 @@ public class DisplayScheduleCommand extends Command {
             break;
         case "today":
             displayScheduleFor(currDate, tasks, ui);
-            ui.printThickSeparator();
-            ui.printBlankLine();
             break;
         case "tomorrow":
             displayScheduleFor(currDate.plus(1, ChronoUnit.DAYS), tasks, ui);
-            ui.printThickSeparator();
-            ui.printBlankLine();
             break;
         default:
             ui.showInvalidDisplayKeywordError();
